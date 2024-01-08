1969c3f57e52f7394f579f697e947dbcca43aa5c
woz07 <105782312+woz07@users.noreply.github.com>
2024-01-08 14:58:02 +0000

    Add files via upload

---
+++ src/main/java/github/woz07/liteconfig/Configuration.java
@@ -0,0 +1,150 @@
+package github.woz07.liteconfig;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author woz07
+ *
+ * Configuration.java
+ * This is the base class of liteconfig.
+ * It is also the class that must be initialized and used.
+ */
+
+public class Configuration {
+    private final File target;
+    private final char delimiter = '=';
+    public Configuration(File target) {
+        if (target == null) {
+            throw new IllegalArgumentException("File cannot be null");
+        }
+        this.target = target;
+    }
+    
+    /**
+     * Add data to the file
+     * @param key   The key you want added
+     * @param value The value you want added paired to the key
+     */
+    public synchronized void set(final String key, final String value) {
+        // Confirm key doesn't already exist to prevent multiple similar keys
+        if (get(key) != null) {
+            throw new IllegalArgumentException("Key already exists within " + target.getName());
+        }
+        // Write out data to file
+        try (FileWriter writer = new FileWriter(target, true)) {
+            writer.write(key + delimiter + value + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Get `value` based on key
+     * @param key   The key you would like to use to get the value
+     * @return      The value associated to the provided key
+     */
+    public synchronized String get(final String key) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(String.valueOf(delimiter));
+                // Check if parts equals to
+                if (parts[0].equals(key)) {
+                    return parts[1];
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    /**
+     * Get rid of key and value from file
+     * @param key   The key you want removed from file (and it's value)
+     */
+    public synchronized void remove(final String key) {
+        List<String> storage = new ArrayList<>();
+        // Add whatever isn't equal to key into storage
+        try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(String.valueOf(delimiter));
+                if (!parts[0].equals(key)) {
+                    storage.add(parts[0] + delimiter + parts[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Write out all the contents of storage to file
+        write(storage);
+    }
+    
+    /**
+     * Update a key from current to a new key
+     * @param old The old key that is within the file
+     * @param replacement The new key you want the old key replaced with
+     */
+    public synchronized void updateKey(final String old, final String replacement) {
+        List<String> storage = new ArrayList<>();
+        // Update old key with replacement key and add it all to storage
+        try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(String.valueOf(delimiter));
+                // Put the replacement, and it's correct value in if target equals parts[0] (key)
+                if (parts[0].equals(old)) {
+                    storage.add(replacement + delimiter + parts[1]);
+                } else {
+                    storage.add(parts[0] + delimiter + parts[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Write out all the contents of storage to file
+        write(storage);
+    }
+    
+    /**
+     * Update a value from current to a new value
+     * @param key The key to access the value
+     * @param replacement The value to get the key from
+     */
+    public synchronized void updateValue(final String key, final String replacement) {
+        List<String> storage = new ArrayList<>();
+        // Update old value with replacement and add it all to storage
+        try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(String.valueOf(delimiter));
+                if (parts[0].equals(key)) {
+                    storage.add(parts[0] + delimiter + replacement);
+                } else {
+                    storage.add(parts[0] + delimiter + parts[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Write out all the contents of storage to file
+        write(storage);
+    }
+    
+    /**
+     * Private method to write data to file (no appending)
+     * @param source Everything within source gets written out
+     */
+    private synchronized void write(List<String> source) {
+        try (FileWriter writer = new FileWriter(target)) {
+            for (String line : source) {
+                writer.write(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/Main.java
@@ -0,0 +1,21 @@
+import github.woz07.liteconfig.Configuration;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration(new File("config.txt"));
+        
+    
+        /*
+        
+        TODO:
+            - Test out for any bugs (use all methods to verify there are non)
+            - Implement more features
+            - Update files from github
+            - Update docs (git & code)
+            - Update release
+            
+         */
+    }
+}
